82a576d22f7de09c57178d7af9cdac697150f16f
pasthortown <pastorchessmaster@gmail.com>
2018-05-25 00:57:23 -0500

    listo

--- server/controladores/especificos/Controlador_login.php
+++ server/controladores/especificos/Controlador_login.php
@@ -9,12 +9,15 @@ class Controlador_login extends Controlador_Base
    { 
        $email = $args["email"];
        $clave = $args["clave"];
-       $sql = "SELECT Persona.*, Cuenta.idRol as 'idRol' FROM Persona INNER JOIN Cuenta ON Cuenta.idPersona = Persona.id WHERE Persona.correoElectronico = ? AND Cuenta.clave = aes_encrypt(?,'".CIFRADO."');";
+       $sql = "SELECT Persona.*, Cuenta.idRol as 'idRol', Cuenta.idEstadoCuenta  FROM Persona INNER JOIN Cuenta ON Cuenta.idPersona = Persona.id WHERE Persona.correoElectronico = ? AND Cuenta.clave = aes_encrypt(?,'".CIFRADO."');";
        $parametros = array($email, $clave);
        $respuesta = $this->conexion->ejecutarConsulta($sql,$parametros);
        if(is_null($respuesta[0])||$respuesta[0]==0){
           return new LoginResult(0,new Persona);
        }else{
+           if($respuesta[0]["idEstadoCuenta"] != 2){
+              return new LoginResult(0,new Persona);
+           }
            $PersonaLogged = new Persona($respuesta[0]["id"],$respuesta[0]["identificacion"],$respuesta[0]["nombres"],$respuesta[0]["apellidos"],$respuesta[0]["idGenero"],$respuesta[0]["direccion"],$respuesta[0]["telefono1"],$respuesta[0]["telefono2"],$respuesta[0]["correoElectronico"]);
            $loginResult = new LoginResult((int)$respuesta[0]["idRol"],$PersonaLogged);
            return $loginResult;
