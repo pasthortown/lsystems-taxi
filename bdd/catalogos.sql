f0eb79113a3362de2808758fd3bfcb76013655f7
pasthortown <pastorchessmaster@gmail.com>
2018-05-25 16:09:21 -0800

    cambios

--- bdd/catalogos.sql
+++ bdd/catalogos.sql
@@ -15,6 +15,7 @@ INSERT INTO `EstadoCuenta` (`id`,`descripcion`) VALUES (3,'Desactivada');
 INSERT INTO `EstadoUnidad` (`id`,`descripcion`) VALUES (1,'Libre');
 INSERT INTO `EstadoUnidad` (`id`,`descripcion`) VALUES (2,'Ocupado');
 INSERT INTO `EstadoUnidad` (`id`,`descripcion`) VALUES (3,'No Disponible');
+INSERT INTO `EstadoUnidad` (`id`,`descripcion`) VALUES (4,'Solicitado');
 INSERT INTO `Coperativa` (`id`,`RUC`,`nombre`,`direccion`,`telefono1`,`telefono2`) VALUES (1,'1720364049001','LSystems Taxi','Altos del Recreo','0998600661','023260618');
 INSERT INTO `Persona` (`id`,`identificacion`,`nombres`,`apellidos`,`idGenero`,`direccion`,`telefono1`,`telefono2`,`correoElectronico`) VALUES (1,'1720364049','Luis Alfonso','Salazar Vaca',1,'CONOCOTO','0998600661','023260618','luissalazarvaca1986@gmail.com');
 INSERT INTO Cuenta (idPersona,idRol,clave,idEstadoCuenta) VALUES (1,1,aes_encrypt('123','lsystems-taxi'),2);
--- lsystems-taxi-admin/src/environments/environment.prod.ts
+++ lsystems-taxi-admin/src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiUrl: 'http://localhost/lsystems-taxi/server/'
+  apiUrl: 'http://192.168.1.181/lsystems-taxi/server/'
 };
