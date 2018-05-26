4b8b66392d721da121a3c6260cdf8fab65b9e998
pasthortown <pastorchessmaster@gmail.com>
2018-05-25 16:13:11 -0800

    login

--- lsystems-taxi-driver/src/app/app.component.ts
+++ lsystems-taxi-driver/src/app/app.component.ts
@@ -1,4 +1,3 @@
-import { TabsPage } from './../pages/tabs/tabs';
 import { LoginPage } from './../pages/login/login';
 import { Component } from '@angular/core';
 import { Platform } from 'ionic-angular';
@@ -9,7 +8,7 @@ import { SplashScreen } from '@ionic-native/splash-screen';
   templateUrl: 'app.html'
 })
 export class MyApp {
-  rootPage:any = TabsPage;
+  rootPage:any = LoginPage;
 
   constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen) {
     platform.ready().then(() => {
