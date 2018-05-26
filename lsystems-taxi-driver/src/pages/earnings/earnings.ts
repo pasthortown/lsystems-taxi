a83f233b0e228b165aa4609df32ed5ceb4d998a6
Pomero1 <31460703+Pomero1@users.noreply.github.com>
2018-05-25 23:26:39 -0500

    Cambios andres

--- lsystems-taxi-driver/src/pages/earnings/earnings.ts
+++ lsystems-taxi-driver/src/pages/earnings/earnings.ts
@@ -14,12 +14,33 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
   templateUrl: 'earnings.html',
 })
 export class EarningsPage {
-
+pasajerosHoy:any[];
+pasajeros:any[];
   constructor(public navCtrl: NavController, public navParams: NavParams) {
-  }
+    this.pasajerosHoy=[];
+    for(let i=1; i<6;i++){
+      this.pasajerosHoy.push({
+        id: i,
+        nombre: 'Nombre '+i,
+        horaInicio: i,
+        horaFin:i,
+      });
+    }
 
+    this.pasajeros=[];
+    for(let i=6; i<17;i++){
+      this.pasajeros.push({
+        id: i,
+        nombre: "Nombre "+i,
+        horaInicio: i,
+        horaFin:i,
+      });
+    }
+  }
+  itemSelected(Item){}
   ionViewDidLoad() {
     console.log('ionViewDidLoad EarningsPage');
   }
+
 
 }
