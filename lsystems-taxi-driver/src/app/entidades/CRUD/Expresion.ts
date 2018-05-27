76f3456d0d7c03d00bfc0cfd9f0d8a683cbe8177
pasthortown <pastorchessmaster@gmail.com>
2018-05-27 16:55:42 -0500

    entidades

--- lsystems-taxi-driver/src/app/entidades/CRUD/Expresion.ts
+++ lsystems-taxi-driver/src/app/entidades/CRUD/Expresion.ts
@@ -1,11 +1,19 @@
 export class Expresion {
-   id: number;
-   idUsuario: number;
-   idUnidad: number;
-   contenido: string;
-   respuesta: string;
-   idCalificacion: number;
-   idAdjunto: number;
-   placa: string;
-   numero: string;
+  id: number;
+  idViaje: number;
+  idUsuario: number;
+  contenido: string;
+  respuesta: string;
+  idCalificacionUnidad: number;
+  idCalificacionConductor: number;
+  idCalificacionEstiloConduccion: number;
+  idCalificacionUsuario: number;
+  idMotivoCalificacionUsuario: number;
+  idMotivoCalificacionUnidad: number;
+  idMotivoCalificacionConductor: number;
+  idMotivoCalificacionEstiloConduccion: number;
+  idAdjunto: number;
+  placa: string;
+  numero: string;
+  idUnidad: number;
 }
