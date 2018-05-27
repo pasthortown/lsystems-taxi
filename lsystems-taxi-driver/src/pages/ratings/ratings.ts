4092a0543507a65172b861b39536441aa09c872f
pasthortown <pastorchessmaster@gmail.com>
2018-05-27 18:52:28 -0500

    estrellitas

--- lsystems-taxi-driver/src/pages/ratings/ratings.ts
+++ lsystems-taxi-driver/src/pages/ratings/ratings.ts
@@ -1,21 +1,23 @@
-import { Component } from '@angular/core';
+import { Unidad } from './../../app/entidades/CRUD/Unidad';
+import { Persona } from './../../app/entidades/CRUD/Persona';
+import { environment } from './../../../environments/environment';
+import { Component, OnInit } from '@angular/core';
 import { IonicPage, NavController, NavParams, ModalController } from 'ionic-angular';
-
-/**
- * Generated class for the RatingsPage page.
- *
- * See https://ionicframework.com/docs/components/#navigation for more info on
- * Ionic pages and navigation.
- */
+import { Http } from '@angular/http';
 
 @IonicPage()
 @Component({
   selector: 'page-ratings',
   templateUrl: 'ratings.html',
 })
-export class RatingsPage {
+export class RatingsPage implements OnInit{
+  calificacion: string;
+  usuario: Persona;
+  unidad: Unidad;
 
-  constructor(public navCtrl: NavController, public navParams: NavParams, private modal: ModalController) {
+  webServiceURL = environment.apiUrl;
+
+  constructor(public navCtrl: NavController, public navParams: NavParams, private modal: ModalController, public http: Http) {
   }
 
   openModal(modalName){
@@ -27,4 +29,32 @@ export class RatingsPage {
 
   }
 
+  ngOnInit() {
+    this.usuario = JSON.parse(sessionStorage.getItem('logedResult')) as Persona;
+    this.unidad = JSON.parse(sessionStorage.getItem('unidad')) as Unidad;
+    this.refresh();
+  }
+
+  refresh() {
+    this.getRating();
+  }
+
+  getRating() {
+    this.http.get(this.webServiceURL + 'expresion/leer_estrellas_conductor?id='+this.usuario.id)
+    .subscribe(r => {
+      if(JSON.stringify(r.json())=='[0]'){
+        this.calificacion = '5';
+        return;
+      }
+      let cuenta = 0;
+      let suma = 0;
+      r.json().forEach(element => {
+        suma += element.Cuenta;
+        cuenta++;
+      });
+      this.calificacion = (suma/cuenta).toFixed(1);
+    }, error => {
+
+    });
+  }
 }
