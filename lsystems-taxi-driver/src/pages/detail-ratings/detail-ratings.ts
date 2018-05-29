8090e8d5fdc78eb7756c26971cefc0b8ee8e6505
pasthortown <pastorchessmaster@gmail.com>
2018-05-29 12:55:49 -0500

    c

--- lsystems-taxi-driver/src/pages/detail-ratings/detail-ratings.ts
+++ lsystems-taxi-driver/src/pages/detail-ratings/detail-ratings.ts
@@ -1,23 +1,66 @@
-import { Component } from '@angular/core';
+import { Persona } from './../../app/entidades/CRUD/Persona';
+import { environment } from './../../../environments/environment';
+import { Component, OnInit } from '@angular/core';
 import { IonicPage, NavController, NavParams, ViewController } from 'ionic-angular';
-
-/**
- * Generated class for the DetailRatingsPage page.
- *
- * See https://ionicframework.com/docs/components/#navigation for more info on
- * Ionic pages and navigation.
- */
+import { Http } from '@angular/http';
 
 @IonicPage()
 @Component({
   selector: 'page-detail-ratings',
   templateUrl: 'detail-ratings.html',
 })
-export class DetailRatingsPage {
-  rate:number
+export class DetailRatingsPage implements OnInit {
+  webServiceURL = environment.apiUrl;
+  usuario: Persona;
+  estrellas1: number;
+  estrellas2: number;
+  estrellas3: number;
+  estrellas4: number;
+  estrellas5: number;
+  total: number;
+  constructor(public navCtrl: NavController, public navParams: NavParams, private view: ViewController, public http: Http) {
 
-  constructor(public navCtrl: NavController, public navParams: NavParams, private view: ViewController) {
-  this.rate=2.5
+  }
+
+  ngOnInit() {
+    this.usuario = JSON.parse(sessionStorage.getItem('logedResult')) as Persona;
+    this.refresh();
+  }
+
+  getEstrellasCuenta() {
+    this.http.get(this.webServiceURL + 'expresion/leer_estrellas_conductor?id='+this.usuario.id)
+    .subscribe(respuesta => {
+      respuesta.json().forEach(element => {
+        if(element.idCalificacionConductor == 1){
+          this.estrellas1 = element.Cuenta;
+        }
+        if(element.idCalificacionConductor == 2){
+          this.estrellas2 = element.Cuenta;
+        }
+        if(element.idCalificacionConductor == 3){
+          this.estrellas3 = element.Cuenta;
+        }
+        if(element.idCalificacionConductor == 4){
+          this.estrellas4 = element.Cuenta;
+        }
+        if(element.idCalificacionConductor == 5){
+          this.estrellas5 = element.Cuenta;
+        }
+        this.total = this.estrellas1 + this.estrellas2 + this.estrellas3 + this.estrellas4 + this.estrellas5;
+      });
+    }, error => {
+
+    });
+  }
+
+  refresh() {
+    this.estrellas1 = 0;
+    this.estrellas2 = 0;
+    this.estrellas3 = 0;
+    this.estrellas4 = 0;
+    this.estrellas5 = 0;
+    this.total = 0;
+    this.getEstrellasCuenta();
   }
 
   closeModal(){
