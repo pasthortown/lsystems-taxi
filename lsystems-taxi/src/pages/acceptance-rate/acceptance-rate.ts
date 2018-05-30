45c5c603d25d3562a8e7a0e5bac1ac7559726027
pasthortown <pastorchessmaster@gmail.com>
2018-05-29 20:41:07 -0500

    c

--- lsystems-taxi/src/pages/acceptance-rate/acceptance-rate.ts
+++ lsystems-taxi/src/pages/acceptance-rate/acceptance-rate.ts
@@ -1,28 +1,61 @@
-import { Component } from '@angular/core';
+import { Persona } from './../../app/entidades/CRUD/Persona';
+import { Component, OnInit } from '@angular/core';
 import { IonicPage, NavController, NavParams, ViewController } from 'ionic-angular';
-
-/**
- * Generated class for the AcceptanceRatePage page.
- *
- * See https://ionicframework.com/docs/components/#navigation for more info on
- * Ionic pages and navigation.
- */
+import { environment } from './../../../environments/environment';
+import { Http } from '@angular/http';
 
 @IonicPage()
 @Component({
   selector: 'page-acceptance-rate',
   templateUrl: 'acceptance-rate.html',
 })
-export class AcceptanceRatePage {
+export class AcceptanceRatePage implements OnInit{
+  acceptance: string;
+  usuario: Persona;
 
-  constructor(public navCtrl: NavController, public navParams: NavParams, private view: ViewController) {
+  webServiceURL = environment.apiUrl;
+
+  constructor(public navCtrl: NavController, public navParams: NavParams, private view: ViewController, public http: Http) {
+  }
+
+  ngOnInit() {
+    this.usuario = JSON.parse(sessionStorage.getItem('logedResult')) as Persona;
+    this.refresh();
+  }
+
+  refresh() {
+    this.getAcceptanceRate();
+  }
+
+  getAcceptanceRate() {
+    this.http.get(this.webServiceURL + 'viaje/leer_estadisticas_tasa_aceptacion_conductor?id='+this.usuario.id)
+    .subscribe(r => {
+      let finalizados = r.json()[0].Finalizados;
+      let asignados = r.json()[0].Asignados;
+      if(asignados == 0 || finalizados == 0){
+        this.acceptance = '100%';
+        return;
+      }
+      let tasa = 100*finalizados/asignados;
+      if(tasa == 100){
+        this.acceptance = '100%';
+        return;
+      }
+      if(tasa == 0){
+        this.acceptance = '0%';
+        return;
+      }
+      this.acceptance = (tasa).toFixed(2) + '%';
+    }, error => {
+
+    });
   }
 
   closeModal(){
     this.view.dismiss();
   }
+
   ionViewDidLoad() {
     console.log('ionViewDidLoad AcceptanceRatePage');
   }
-
 }
--- lsystems-taxi/src/pages/detail-ratings/detail-ratings.ts
+++ lsystems-taxi/src/pages/detail-ratings/detail-ratings.ts
@@ -1,23 +1,76 @@
-import { Component } from '@angular/core';
+import { Persona } from './../../app/entidades/CRUD/Persona';
+import { environment } from './../../../environments/environment';
+import { Component, OnInit } from '@angular/core';
 import { IonicPage, NavController, NavParams, ViewController } from 'ionic-angular';
-
-/**
- * Generated class for the DetailRatingsPage page.
- *
- * See https://ionicframework.com/docs/components/#navigation for more info on
- * Ionic pages and navigation.
- */
+import { Http } from '@angular/http';
 
 @IonicPage()
 @Component({
   selector: 'page-detail-ratings',
   templateUrl: 'detail-ratings.html',
 })
-export class DetailRatingsPage {
-  rate:number
+export class DetailRatingsPage implements OnInit {
+  webServiceURL = environment.apiUrl;
+  usuario: Persona;
+  estrellas1: number;
+  estrellas2: number;
+  estrellas3: number;
+  estrellas4: number;
+  estrellas5: number;
+  estrellas1p: number;
+  estrellas2p: number;
+  estrellas3p: number;
+  estrellas4p: number;
+  estrellas5p: number;
+  total: number;
+  constructor(public navCtrl: NavController, public navParams: NavParams, private view: ViewController, public http: Http) {
 
-  constructor(public navCtrl: NavController, public navParams: NavParams, private view: ViewController) {
-  this.rate=2.5
+  }
+
+  ngOnInit() {
+    this.usuario = JSON.parse(sessionStorage.getItem('logedResult')) as Persona;
+    this.refresh();
+  }
+
+  getEstrellasCuenta() {
+    this.http.get(this.webServiceURL + 'expresion/leer_estrellas_conductor?id='+this.usuario.id)
+    .subscribe(respuesta => {
+      respuesta.json().forEach(element => {
+        if(element.idCalificacionConductor == 1){
+          this.estrellas1 = element.Cuenta;
+        }
+        if(element.idCalificacionConductor == 2){
+          this.estrellas2 = element.Cuenta;
+        }
+        if(element.idCalificacionConductor == 3){
+          this.estrellas3 = element.Cuenta;
+        }
+        if(element.idCalificacionConductor == 4){
+          this.estrellas4 = element.Cuenta;
+        }
+        if(element.idCalificacionConductor == 5){
+          this.estrellas5 = element.Cuenta;
+        }
+      });
+      this.total = this.estrellas1*1 + this.estrellas2*1 + this.estrellas3*1 + this.estrellas4*1 + this.estrellas5*1;
+      this.estrellas1p = (this.estrellas1/this.total)*100;
+      this.estrellas2p = (this.estrellas2/this.total)*100;
+      this.estrellas3p = (this.estrellas3/this.total)*100;
+      this.estrellas4p = (this.estrellas4/this.total)*100;
+      this.estrellas5p = (this.estrellas5/this.total)*100;
+    }, error => {
+
+    });
+  }
+
+  refresh() {
+    this.estrellas1 = 0;
+    this.estrellas2 = 0;
+    this.estrellas3 = 0;
+    this.estrellas4 = 0;
+    this.estrellas5 = 0;
+    this.total = 0;
+    this.getEstrellasCuenta();
   }
 
   closeModal(){
