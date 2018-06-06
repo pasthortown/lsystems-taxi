4576ed98ba8cebb7a6453169b057a8a8cecb0a82
pasthortown <pastorchessmaster@gmail.com>
2018-06-06 12:54:03 -0500

    ok

--- lsystems-taxi/src/pages/search/search.ts
+++ lsystems-taxi/src/pages/search/search.ts
@@ -1,13 +1,6 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams, ViewController } from 'ionic-angular';
 
-/**
- * Generated class for the SearchPage page.
- *
- * See https://ionicframework.com/docs/components/#navigation for more info on
- * Ionic pages and navigation.
- */
-
 @IonicPage()
 @Component({
   selector: 'page-search',
@@ -22,11 +15,7 @@ export class SearchPage {
   }
 
   initializeItems() {
-    this.items = [
-      'ejemplo 1',
-      'ejemplo 2',
-      'ejemplo 3'
-  ];
+
   }
 
   useItem(item) {
@@ -34,18 +23,7 @@ export class SearchPage {
   }
 
   getItems(ev) {
-    // Reset items back to all of the items
-    this.initializeItems();
 
-    // set val to the value of the ev target
-    var val = ev.target.value;
-
-    // if the value is an empty string don't filter the items
-    if (val && val.trim() != '') {
-      this.items = this.items.filter((item) => {
-        return (item.toLowerCase().indexOf(val.toLowerCase()) > -1);
-      })
-    }
   }
 
   ionViewDidLoad() {
